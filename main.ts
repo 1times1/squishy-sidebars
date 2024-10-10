04f17be13cbf2a3881bc6b9796a1453c16bdbacc
greenworm <he@llo.dev>
2024-10-10 17:55:11 +1100

    deleted unused code

--- main.ts
+++ main.ts
@@ -17,67 +17,26 @@ export default class Squishy extends Plugin {
 			return l;
 		};
 
+		//debug command
 		/* {
-				id: "brfind",
-				name: "",
-				editorCheckCallback: (checking: boolean, editor: Editor, info: MarkdownView | MarkdownFileInfo) => batchRenameCallback("findreplace", checking, editor, info),
-			} */ /* {
-				id: "getfiletab",
-				name: "Open a file tab in the centre",
-				callback: () => {
-					const l = this.app.workspace.getLeaf("tab");
-					l.setViewState({ type: "file-explorer", active: true})
-					//@ts-ignore
-					.then(() => l.view.closeable = true);
-				}
-			} */
-
-				/* {
-				id: "getsearchtab",
-				name: "Open a search tab in the centre",
-				callback: () => {
-					const l = this.app.workspace.getLeaf("tab");
-					l.setViewState({ type: "search", active: true})
-					//@ts-ignore
-					.then(() => l.view.closeable = true);
-				}
-			}, {
 				id: "help",
 				name: "help",
 				callback: () => this.app.workspace.iterateAllLeaves((leaf) => console.log(leaf))
 			} */
 
-		//@ts-ignore
-		/* const ogESL = this.app.workspace.ensureSideLeaf.bind(this.app.workspace); //originalEnsureSideLeaf
-		//@ts-ignore
-		this.app.workspace.ensureSideLeaf = function (a:any, b:any, c:any) {
-			//f (finished) {
-				const leaf = ogESL(a,b,c);
-				//waitPatch(leaf);
-				
-			//}
-			if (!finished) {
-				leafarray.push(leaf);
-			}
-			return leaf;
-			return undefined
-			}; */
-
-		//this.app.workspace.getRightLeaf = this.app.workspace.getLeftLeaf;
-
 		this.app.workspace.onLayoutReady(() => {
 			//set finished to true so no more pushing to array
 			finished = true;
-			//
+			
+			//debug code - if you want to see the view type strings just check workspace.json
 			//this.app.workspace.iterateAllLeaves((l) => console.log(l.view.getViewType()))
 
 			let leavesIMust = this.app.workspace.getLeavesOfType("file-explorer");
 			leavesIMust.push(...this.app.workspace.getLeavesOfType("search"));
 			leavesIMust.push(...this.app.workspace.getLeavesOfType("tag"));
-			leavesIMust.forEach((v) => {
-				patch(v);
-			}
-			)
+
+			leavesIMust.forEach((v) => patch(v));
+
 			leafarray.forEach((l) => l.detach()) //detach all the leaves in the array
 
 			
