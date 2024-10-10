ccf63d44568c0cae873a8b1af8f614ba06066262
greenworm <he@llo.dev>
2024-10-10 17:53:08 +1100

    copied code from my other plugin which this grew like a tumor on

--- main.ts
+++ main.ts
@@ -1,134 +1,116 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { WorkspaceLeaf, Plugin, ItemView } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class Squishy extends Plugin {
+	
 	async onload() {
-		await this.loadSettings();
+		let finished = false; //will be set to true on LayoutReady
+		let leafarray: WorkspaceLeaf[] = [];
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		//@ts-ignore
+		this.app.workspace.getSideLeaf = (a: any, b: any) => { //if obsidian tries to open a side leaf, force it to the center
+			let l = this.app.workspace.getLeaf("tab");
+			patch(l);
+			if (!finished) { //if not finished...
+				leafarray.push(l); //add the leaf into the array
+				//so any side leaves obsidian tries to open before workspacelayout ready will be in the array
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			return l;
+		};
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		/* {
+				id: "brfind",
+				name: "",
+				editorCheckCallback: (checking: boolean, editor: Editor, info: MarkdownView | MarkdownFileInfo) => batchRenameCallback("findreplace", checking, editor, info),
+			} */ /* {
+				id: "getfiletab",
+				name: "Open a file tab in the centre",
+				callback: () => {
+					const l = this.app.workspace.getLeaf("tab");
+					l.setViewState({ type: "file-explorer", active: true})
+					//@ts-ignore
+					.then(() => l.view.closeable = true);
 				}
+			} */
+
+				/* {
+				id: "getsearchtab",
+				name: "Open a search tab in the centre",
+				callback: () => {
+					const l = this.app.workspace.getLeaf("tab");
+					l.setViewState({ type: "search", active: true})
+					//@ts-ignore
+					.then(() => l.view.closeable = true);
+				}
+			}, {
+				id: "help",
+				name: "help",
+				callback: () => this.app.workspace.iterateAllLeaves((leaf) => console.log(leaf))
+			} */
+
+		//@ts-ignore
+		/* const ogESL = this.app.workspace.ensureSideLeaf.bind(this.app.workspace); //originalEnsureSideLeaf
+		//@ts-ignore
+		this.app.workspace.ensureSideLeaf = function (a:any, b:any, c:any) {
+			//f (finished) {
+				const leaf = ogESL(a,b,c);
+				//waitPatch(leaf);
+				
+			//}
+			if (!finished) {
+				leafarray.push(leaf);
 			}
+			return leaf;
+			return undefined
+			}; */
+
+		//this.app.workspace.getRightLeaf = this.app.workspace.getLeftLeaf;
+
+		this.app.workspace.onLayoutReady(() => {
+			//set finished to true so no more pushing to array
+			finished = true;
+			//
+			//this.app.workspace.iterateAllLeaves((l) => console.log(l.view.getViewType()))
+
+			let leavesIMust = this.app.workspace.getLeavesOfType("file-explorer");
+			leavesIMust.push(...this.app.workspace.getLeavesOfType("search"));
+			leavesIMust.push(...this.app.workspace.getLeavesOfType("tag"));
+			leavesIMust.forEach((v) => {
+				patch(v);
+			}
+			)
+			leafarray.forEach((l) => l.detach()) //detach all the leaves in the array
+
+			
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
 	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+
+class WowView extends ItemView { //implemented version of itemview used to make file explorer moveable
+	leaf: WorkspaceLeaf;
+	
+	constructor(l: WorkspaceLeaf) {
+		super(l);
+		this.leaf = l;
+		
+		this.containerEl.detach(); //stop ItemView artifacts from showing up
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+	getDisplayText(): string {
+		return this.leaf.view.getDisplayText();
+	};
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+	getViewType(): string {
+		return this.leaf.view.getViewType();
+	};
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+function patch(l: WorkspaceLeaf) {
+	Object.getPrototypeOf(l).canClose = () => true;
+	Object.setPrototypeOf(Object.getPrototypeOf(l.view), new WowView(l));
 }
